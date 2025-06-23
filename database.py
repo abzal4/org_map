708935bb172323e8b06a3fa44d6f87a8b7f93c78
abzal4 <abzaltleules15@gmail.com>
2025-06-23 10:59:07 +0500

    Initial commit

---
+++ database.py
@@ -0,0 +1,20 @@
+import pandas as pd
+from pymongo import MongoClient
+
+# Подключение к MongoDB (локально или Atlas)
+client = MongoClient("mongodb://localhost:27017")  # или URI MongoDB Atlas
+db = client["practice"]
+collection = db["for_map"]
+
+collection.delete_many({})
+
+# Чтение CSV
+df = pd.read_csv("tomi_map - Лист1.csv")  # Убедись, что файл в том же каталоге
+
+# Преобразование и загрузка
+data = df.to_dict(orient="records")
+collection.insert_many(data)
+
+
+
+print("✅ Данные успешно загружены в MongoDB!")
